9231644191a2e735e1d49536b998a6d3d79661a3
Nanda Gopal Pattanayak <40311776+NandaGopal56@users.noreply.github.com>
2024-04-15 00:06:04 +0530

    version setup

---
+++ todo/cli.py
@@ -0,0 +1,18 @@
+from typing import Optional
+import typer
+from todo import __app_name__, __version__
+
+app = typer.Typer()
+
+def _version_callback(value: bool) -> None:
+    if value:
+        typer.echo(f"{__app_name__} v{__version__}")
+        raise typer.Exit(0)
+    
+@app.callback()
+def main(
+    version: Optional[bool] = typer.Option(
+        None, "--version", '-v', help='Show the ap version no. & exit', callback=_version_callback, is_eager=True
+    )
+) -> None:
+    return
